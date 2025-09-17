e21a96ca4a6b1c3de89b6c1e5ddecc0e72605a64
DrBidoof <alwyn.lynch23@gmail.com>
2025-09-17 14:54:40 -0400

    q1
    q1 test

---
+++ AlwynLynch_COMP254Lab1/LinkedList/Q1/DoublelyLinkedList.py
@@ -0,0 +1,88 @@
+class Node:
+    __slots__ = ("element", "prev", "next")
+
+    def __init__(self, element=None, prev=None, next=None):
+        self.element = element
+        self.prev = prev
+        self.next = next
+
+
+class DoublyLinkedList:
+    """Doubly linked list with header/trailer sentinels."""
+
+    def __init__(self):
+        self.header = Node()               # sentinel at front
+        self.trailer = Node()              # sentinel at back
+        self.header.next = self.trailer
+        self.trailer.prev = self.header
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def _insert_between(self, e, predecessor, successor):
+        """Insert element e between two existing nodes."""
+        newest = Node(e, predecessor, successor)
+        predecessor.next = newest
+        successor.prev = newest
+        self.size += 1
+        return newest
+
+    def _delete_node(self, node):
+        """Remove a non-sentinel node and return its element."""
+        pred, succ = node.prev, node.next
+        pred.next = succ
+        succ.prev = pred
+        self.size -= 1
+        elem = node.element
+        node.prev = node.next = node.element = None
+        return elem
+
+    def add_first(self, e):
+        return self._insert_between(e, self.header, self.header.next)
+
+    def add_last(self, e):
+        return self._insert_between(e, self.trailer.prev, self.trailer)
+
+    def remove_first(self):
+        if self.is_empty():
+            raise IndexError("remove_first from empty list")
+        return self._delete_node(self.header.next)
+
+    def remove_last(self):
+        if self.is_empty():
+            raise IndexError("remove_last from empty list")
+        return self._delete_node(self.trailer.prev)
+
+    def __iter__(self):
+        cursor = self.header.next
+        while cursor is not self.trailer:
+            yield cursor.element
+            cursor = cursor.next
+
+    def __repr__(self):
+        return f"DoublyLinkedList([{', '.join(repr(x) for x in self)}])"
+
+    def concatenate(self, other):
+        """Concatenate list 'other' to the end of this list."""
+        if other.is_empty():
+            return
+
+        last_L = self.trailer.prev
+        first_M = other.header.next
+        last_M = other.trailer.prev
+
+        last_L.next = first_M
+        first_M.prev = last_L
+
+        self.trailer.prev = last_M
+        last_M.next = self.trailer
+
+        self.size += other.size
+
+        other.header.next = other.trailer
+        other.trailer.prev = other.header
+        other.size = 0
--- AlwynLynch_COMP254Lab1/LinkedList/Q1/Q1.py
+++ AlwynLynch_COMP254Lab1/LinkedList/Q1/Q1.py
@@ -1,1 +1,20 @@
-print("hello world");
+from DoublelyLinkedList import DoublyLinkedList
+
+if __name__ == "__main__":
+    L = DoublyLinkedList()
+    M = DoublyLinkedList()
+
+    for x in (1, 2, 3):
+        L.add_last(x)
+    for x in (4, 5, 6):
+        M.add_last(x)
+
+    print("Before:")
+    print("L:", L)
+    print("M:", M)
+
+    L.concatenate(M)
+
+    print("\nAfter concatenate:")
+    print("L:", L)
+    print("M:", M)
