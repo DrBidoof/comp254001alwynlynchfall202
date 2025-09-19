e497509c90e4241acabf106020e3753fa1db0b02
DrBidoof <alwyn.lynch23@gmail.com>
2025-09-18 20:15:39 -0400

    Update CircularlyLinkedList.py
    update

--- AlwynLynch_COMP254Lab1/LinkedList/Q3/CircularlyLinkedList.py
+++ AlwynLynch_COMP254Lab1/LinkedList/Q3/CircularlyLinkedList.py
@@ -1,6 +1,8 @@
-class my_class(object):
-    pass
+class CircularlyLinkedList:
+    """Circularly linked list storing elements; tail points to last node, tail.next is head."""
 
-
-
-
+    class _Node:
+        __slots__ = ("element", "next")
+        def __init__(self, element, next=None):
+            self.element = element
+            self.next = next
